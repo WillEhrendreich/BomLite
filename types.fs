ac490fcd3cd282c142f026ca329af4639b7b04aa
Will Ehrendreich <will.ehrendreich@gmail.com>
2023-12-06 21:23:58 -0600

    feat(types): types now better align with database columns

--- types.fs
+++ types.fs
@@ -1,6 +1,7 @@
 namespace bomLite
+open System
 
-type ItemMaster(partNumber,description,unitOfMeasure,unitPrice,revisionNumber) =
+type ItemMaster(partNumber,description,unitOfMeasure,unitPrice,revisionNumber,identity) =
   member val PartNumber:string = partNumber with get, set 
   member val Description:string = description with get, set
   member val UnitOfMeasure:string =  unitOfMeasure with get, set
@@ -8,17 +9,19 @@ type ItemMaster(partNumber,description,unitOfMeasure,unitPrice,revisionNumber) =
   member val RevisionNumber:string = revisionNumber with get, set 
   member val IdentityColumn:int = identity with get, set 
 
-type JobOrderMaster(partNumber,description,unitOfMeasure,unitPrice,quantity,revisionNumber,jobNumber) =
+type JobOrderMaster(jobNumber,description,salesOrderNumber,parentJobNumber,hasSubJobs,companyName,orderDate,dueDate,status,identity) =
   member val JobNumber:string = jobNumber with get, set 
-  member val PartNumber:string = partNumber with get, set 
   member val Description:string = description with get, set
-  member val UnitOfMeasure:string =  unitOfMeasure with get, set
-  member val UnitPrice :decimal= unitPrice with get, set 
-  member val Quantity :decimal=   quantity with get, set 
-  member val RevisionNumber:string = revisionNumber with get, set 
+  member val SalesOrder:string = salesOrderNumber with get, set 
+  member val ParentJobNumber:string = parentJobNumber with get, set 
+  member val HasSubJobs :bool=   hasSubJobs with get, set 
+  member val CompanyName:string = companyName with get, set 
+  member val OrderDate:DateTime = orderDate with get, set 
+  member val DueDate:DateTime = dueDate with get, set 
+  member val Status:string = status with get, set 
   member val IdentityColumn:int = identity with get, set 
 
-type JobOrderRow(partNumber,description,unitOfMeasure,unitPrice,quantity,revisionNumber,jobNumber,itemNumber) =
+type JobOrderRow(partNumber,description,unitOfMeasure,unitPrice,quantity,revisionNumber,jobNumber,itemNumber,identity) =
   member val ItemNumber:string = itemNumber with get, set 
   member val JobNumber:string = jobNumber with get, set 
   member val PartNumber:string = partNumber with get, set 
